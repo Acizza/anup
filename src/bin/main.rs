4a1dae1099130ae9d6cfbcbd3627335b407b05ae
Acizza <jonathanmce@gmail.com>
2017-11-27 19:04:41 -0800

    Add main::get_mal_list_entry Add prompt::add_to_anime_list Add prompt::input::read_yn

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,7 @@ extern crate failure_derive;
 #[macro_use]
 extern crate lazy_static;
 
+extern crate chrono;
 extern crate mal;
 extern crate regex;
 
@@ -12,32 +13,11 @@ mod prompt;
 mod process;
 mod series;
 
-use std::path::Path;
 use failure::{Error, ResultExt};
 use mal::MAL;
+use mal::list::AnimeEntry;
 use series::{EpisodeData, Series};
-
-fn get_series_data(mal: &MAL, path: &Path) -> Result<Series, Error> {
-    let loc_data = EpisodeData::parse(path)?;
-    let info = prompt::find_and_select_series(mal, &loc_data.series_name)?;
-
-    Ok(Series::new(info, loc_data))
-}
-
-fn run(args: Vec<String>) -> Result<(), Error> {
-    let mal = MAL::new(args[2].clone(), args[3].clone());
-    let series = get_series_data(&mal, Path::new(&args[1]))?;
-
-    let anime_list = mal.get_anime_list().context("anime list retrieval failed")?;
-
-    if let Some(list_status) = anime_list.iter().find(|a| a.info.id == series.info.id) {
-        println!("found anime on anime list:\n{:?}", list_status);
-    } else {
-        println!("anime not found on anime list");
-    }
-
-    Ok(())
-}
+use std::path::Path;
 
 fn main() {
     // Temporary
@@ -57,4 +37,30 @@ fn main() {
             eprintln!("{}", e.backtrace());
         }
     }
+}
+
+fn run(args: Vec<String>) -> Result<(), Error> {
+    let mal = MAL::new(args[2].clone(), args[3].clone());
+    let series = get_series_data(&mal, Path::new(&args[1]))?;
+
+    let entry = get_mal_list_entry(&mal, &series)?;
+    println!("{:?}", entry);
+
+    Ok(())
+}
+
+fn get_mal_list_entry(mal: &MAL, series: &Series) -> Result<AnimeEntry, Error> {
+    let list = mal.get_anime_list().context("anime list retrieval failed")?;
+
+    match list.into_iter().find(|e| e.info.id == series.info.id) {
+        Some(entry) => Ok(entry),
+        None => prompt::add_to_anime_list(mal, series),
+    }
+}
+
+fn get_series_data(mal: &MAL, path: &Path) -> Result<Series, Error> {
+    let loc_data = EpisodeData::parse(path)?;
+    let info = prompt::find_and_select_series(mal, &loc_data.series_name)?;
+
+    Ok(Series::new(info, loc_data))
 }
--- src/bin/prompt.rs
+++ src/bin/prompt.rs
@@ -1,5 +1,9 @@
+use chrono::{Local, NaiveDate};
 use failure::{Error, ResultExt};
 use mal::{SeriesInfo, MAL};
+use mal::list::{AnimeEntry, EntryTag, Status};
+use Series;
+use std;
 
 mod input {
     use failure::Error;
@@ -23,7 +27,36 @@ mod input {
             }
         }
     }
+
+    #[derive(Debug)]
+    pub enum Answer {
+        Yes,
+        No,
+    }
+
+    impl Into<bool> for Answer {
+        fn into(self) -> bool {
+            match self {
+                Answer::Yes => true,
+                Answer::No => false,
+            }
+        }
+    }
+
+    pub fn read_yn(default: Answer) -> Result<bool, Error> {
+        let line = read_line()?;
+
+        let answer = match line.as_str() {
+            "Y" | "y" => true,
+            "N" | "n" => false,
+            _ => default.into(),
+        };
+
+        Ok(answer)
+    }
 }
+
+use self::input::Answer;
 
 pub fn find_and_select_series(mal: &MAL, name: &str) -> Result<SeriesInfo, Error> {
     let mut series = mal.search(name).context("MAL search failed")?;
@@ -43,4 +76,38 @@ pub fn find_and_select_series(mal: &MAL, name: &str) -> Result<SeriesInfo, Error
     } else {
         Ok(series.swap_remove(0))
     }
+}
+
+pub fn add_to_anime_list(mal: &MAL, series: &Series) -> Result<AnimeEntry, Error> {
+    println!(
+        "[{}] is not on your anime list\ndo you want to add it? (Y/n)",
+        series.info.title
+    );
+
+    if input::read_yn(Answer::Yes)? {
+        let today = get_today_naive();
+
+        mal.add_anime(
+            series.info.id,
+            &[
+                EntryTag::Status(Status::Watching),
+                EntryTag::StartDate(today),
+            ],
+        )?;
+
+        Ok(AnimeEntry {
+            info: series.info.clone(),
+            watched_episodes: 0,
+            start_date: Some(today),
+            end_date: None,
+            status: Status::Watching,
+        })
+    } else {
+        // No point in continuing in this case
+        std::process::exit(0);
+    }
+}
+
+fn get_today_naive() -> NaiveDate {
+    Local::today().naive_utc()
 }
