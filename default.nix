ddc78ac71e0c1b5a2e7ecf07d8b3a4ae66ae155c
Acizza <jonathanmce@pm.me>
2018-07-14 16:55:43 -0700

    [meta] Add script to compile the program more easily on NixOS

---
+++ default.nix
@@ -0,0 +1,9 @@
+# This script is used for building the program on NixOS with the proper dependencies.
+# To use, simply run nix-shell in this directory and run "cargo build" as you normally would.
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "anup";
+    buildInputs = [ pkgconfig openssl zlib ];
+}
