6a3d3017d6a356f8a5027941f5bd7de95631e08d
Acizza <jonathanmce@pm.me>
2018-07-23 01:53:05 -0700

    [meta] Add xdg_utils to built inputs for NixOS script

--- default.nix
+++ default.nix
@@ -5,5 +5,5 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
     name = "anup";
-    buildInputs = [ buildPackages.stdenv.cc pkgconfig openssl.dev ];
+    buildInputs = [ buildPackages.stdenv.cc pkgconfig openssl.dev xdg_utils ];
 }
