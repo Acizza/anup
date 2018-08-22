7c94c54182182640576fb8f7bb1255376d695c9a
Acizza <jonathanmce@pm.me>
2018-08-22 04:32:00 -0700

    [meta] Remove default.nix script for NixOS
    Considering how niche NixOS is, it seems like a better idea to leave the build script out.

--- default.nix
+++
@@ -1,9 +0,0 @@
-# This script is used for building the program on NixOS with the proper dependencies.
-# To use, simply run nix-shell in this directory and run "cargo build" as you normally would.
-
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-    name = "anup";
-    buildInputs = [ buildPackages.stdenv.cc pkgconfig openssl.dev xdg_utils ];
-}
