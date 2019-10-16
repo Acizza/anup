7f3c45ad1b85aa0c0d8085cea3f9ee3b550e15d9
Acizza <jgit@tuta.io>
2019-10-16 15:04:46 -0700

    anime: add documentation to remote module

--- anime/src/remote/mod.rs
+++ anime/src/remote/mod.rs
@@ -6,19 +6,39 @@ use serde_derive::{Deserialize, Serialize};
 use std::borrow::Cow;
 use std::fmt;
 
+/// Core functionality to interact with an anime tracking service.
 pub trait RemoteService: ScoreParser {
+    /// Search for an anime's information by title and return all of the matches.
     fn search_info_by_name(&self, name: &str) -> Result<Vec<SeriesInfo>>;
+
+    /// Get an anime's information by its ID.
+    /// Note that the ID will differ from service to service.
     fn search_info_by_id(&self, id: u32) -> Result<SeriesInfo>;
 
+    /// Retrieve the anime list entry from the currently authenticated user.
+    ///
+    /// `id` is the ID of the anime, which differs from service to service.
     fn get_list_entry(&self, id: u32) -> Result<Option<SeriesEntry>>;
+
+    /// Upload `entry` to the currently authenticated user's anime list.
+    ///
+    /// Please ensure that the `SeriesEntry` you are using comes from the current service
+    /// in use, or you may overwrite a completely different list entry.
     fn update_list_entry(&self, entry: &SeriesEntry) -> Result<()>;
 
+    /// Indicates whether or not this service is meant to be used without an internet connection.
+    ///
+    /// Returns false by default.
     fn is_offline(&self) -> bool {
         false
     }
 }
 
+/// Functionality to deal with scores from an anime tracking service.
 pub trait ScoreParser {
+    /// Parse the given `score` string to a u8 between 0 - 100.
+    ///
+    /// By default, it will simply map `score` to its equivalent u8 value.
     fn parse_score(&self, score: &str) -> Option<u8> {
         score
             .parse()
@@ -26,6 +46,9 @@ pub trait ScoreParser {
             .and_then(|score| if score <= 100 { Some(score) } else { None })
     }
 
+    /// Map the given `score` to its string equivalent.
+    ///
+    /// By default, it will simply return `score` as a string.
     fn score_to_str(&self, score: u8) -> Cow<str> {
         Cow::Owned(score.to_string())
     }
@@ -33,12 +56,18 @@ pub trait ScoreParser {
 
 pub type Minutes = u32;
 
+/// General information for an anime series.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SeriesInfo {
+    /// The ID of the series.
     pub id: u32,
+    /// The titles of the series.
     pub title: SeriesTitle,
+    /// The number of episodes.
     pub episodes: u32,
+    /// The length of a single episode in minutes.
     pub episode_length: Minutes,
+    /// An ID pointing to the sequel of this series.
     pub sequel: Option<u32>,
 }
 
@@ -54,24 +83,37 @@ impl<'a> Into<Cow<'a, SeriesInfo>> for &'a SeriesInfo {
     }
 }
 
+/// Various title formats for an anime series.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct SeriesTitle {
+    /// The title in romaji.
     pub romaji: String,
+    /// The title in the user's preferred format.
     pub preferred: String,
 }
 
+/// A list entry for an anime series.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct SeriesEntry {
+    /// The ID of the anime.
     pub id: u32,
+    /// The number of episodes that have been watched.
     pub watched_eps: u32,
+    /// The score given by the user.
     pub score: Option<u8>,
+    /// The user's current watch status of the series.
     pub status: Status,
+    /// The number of times the user has rewatched the series.
     pub times_rewatched: u32,
+    /// The date the user started watching the series.
     pub start_date: Option<chrono::NaiveDate>,
+    /// The date the user finished watching the series.
     pub end_date: Option<chrono::NaiveDate>,
 }
 
 impl SeriesEntry {
+    /// Create a new `SeriesEntry` associated to the anime with the specified `id`.
+    #[inline]
     pub fn new(id: u32) -> SeriesEntry {
         SeriesEntry {
             id,
@@ -85,6 +127,7 @@ impl SeriesEntry {
     }
 }
 
+/// The watch status of an anime series.
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 pub enum Status {
     Watching,
