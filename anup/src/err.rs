60433d414edd88c3abaf3ea1c17f1adea389f849
Acizza <jgit@tuta.io>
2020-10-08 15:04:47 -0700

    anup: Fix clippy warning

--- anup/src/err.rs
+++ anup/src/err.rs
@@ -2,8 +2,5 @@ use anyhow::Error;
 use std::io;
 
 pub fn is_file_nonexistant(err: &Error) -> bool {
-    match err.downcast_ref::<io::Error>() {
-        Some(err) if err.kind() == io::ErrorKind::NotFound => true,
-        _ => false,
-    }
+    matches!(err.downcast_ref::<io::Error>(), Some(err) if err.kind() == io::ErrorKind::NotFound)
 }
