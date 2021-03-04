1c69723b746b5c194ae529d823810a608ec5c8ea
Acizza <jgit@tuta.io>
2021-03-04 14:21:00 -0800

    Add latest stable Rust version to default.nix buildInputs

--- default.nix
+++ default.nix
@@ -1,5 +1,19 @@
-with import <nixpkgs> {};
+{}:
 
-pkgs.mkShell {
-    buildInputs = [ stdenv.cc pkgconfig sqlite.dev xdg_utils ];
+let
+  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+
+  pkgs = import <nixpkgs> {
+    overlays = [ rustOverlay ];
+  };
+
+  rust = pkgs.rust-bin.stable.latest.rust.override {
+    extensions = [ "rust-src" ];
+  };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rust
+    sqlite.dev
+    xdg_utils
+  ];
 }
